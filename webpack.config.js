576c4fcf2fd9d3079f05a448d9db08515ed8b9a4
mavennn <leshich417@gmail.com>
2020-02-08 21:40:13 +0300

    migrate to typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,65 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
-const dev = {
-    entry: {
-        main: "./src/index.js",
-    },
+const config = {
+    entry: "./src/index.tsx",
+    mode: "development",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js"]
+    },
+
     module: {
         rules: [
-
-            // JS and JSX files loader
             {
-                test: /\.(js|jsx)$/,
+                test: /\.ts(x?)$/,
                 exclude: /node_modules/,
-                use: ["babel-loader"]
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(css|s[ac]ss)$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
             },
 
-            // CSS files loader
             {
-                test: /\.(css)$/,
-                use: ["style-loader", "css-loader"]
+                test: /\.html$/,
+                use: ["html-loader"]
             },
-
-            // HTML files loader
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             {
-                test: /\.(html)$/,
-                use: {
-                    loader: "html-loader"
-                }
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
             }
         ]
     },
 
-    // HTML Webpack Plugin for produce React components to HTML page
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./public/index.html",
+            template: "./src/index.html",
             filename: "./index.html"
         })
-    ]
+    ],
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
 };
 
-module.exports = [dev];
+module.exports = config;
